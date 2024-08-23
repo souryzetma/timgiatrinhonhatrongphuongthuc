529d25cd77d13abe1ec6a515e39e6d4fad06b40d
souryzetma <91234492+souryzetma@users.noreply.github.com>
2024-08-23 18:48:40 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿﻿using System;
+
+namespace GTNN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] arr = {4, 12, 7, 8, 1, 0, 9};
+           int index = MinValue(arr);
+           Console.WriteLine("The smallest element in the array is: " + arr[index]);
+        }
+
+        private static int MinValue(int[] array)
+        {
+            int min = array[0];
+            int index = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                    index = 1;
+                }
+            }
+            return index;
+        }
+    }
+}
